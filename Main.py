4ee0dd672fa7dd133c886c3f6b5244fd9ab439ed
SonnySkyez <hnic85@gmail.com>
2019-06-12 17:04:04 +0200

    Comments edition, erasing blank lines of code. Minor fixes

--- Main.py
+++ Main.py
@@ -26,21 +26,14 @@ from functions import date_change, get_country_code,impress_check, calculate_cli
 '''Search in current working directory for .csv file.
 If found nothing: wrote proper error to Stderr.'''
 
-
 list_DIR = os.listdir('.')    # list files in CWD
 input_path = ''
-
-
 print('Searching for a .csv input file in current directory.')
 
-
 for search_file in list_DIR:
-
     if not search_file.endswith('.csv'):
-        continue    # skip, if it's different file type
-
+        continue              # skip, if it's different file type
     elif search_file.endswith('.csv'):
-
         print('{} file has been found.'.format(search_file))
         input_path = search_file
         break
@@ -49,106 +42,71 @@ if len(input_path) == 0:
     sys.stderr.write('STDERR: Critical error. No .csv file was found in working directory.\n'
                      'Copy input file in .csv format to this directory and try again.\n')
     sys.exit()
-
 else:
     pass
-
 
 '''Detect if the .csv file encoding is utf-8 or other.
 If file is encoded in other encoding, e.g utf-16 then
 write proper error into Stderr.
 Detect if the input file is empty file.'''
 
-
 print('Detecting {} file encoding.'.format(input_path))
-
-
 raw_data = open(input_path, 'rb').read()
 encoding = chardet.detect(raw_data)
-
 
 if encoding['encoding'] == 'utf-8' or encoding['encoding'] == 'ascii':
     print('Detection succeeded. File encoding format is valid')
     pass
-
 else:
     sys.stderr.write('STDERR: Critical error. Input file empty or not encoded '
                      'in UTF-8 encoding. This script supports only utf-8 encoding.\n')
     sys.exit()
 
-
 '''Read .csv file with csv reader from standard Python module,
 make changes in rows to match output standard: 2019-01-21,AFG,919,6
 List rows lexicographically in "input_Rows" list'''
 
-
 print('Opening .csv file and modifying it line by line.')
-
-
 input_Rows = []
-
 input_CSV = open(input_path, newline='', encoding='utf-8')
 input_Reader = csv.reader(input_CSV)
 
-
 try:
-
     for row in input_Reader:
-
         if not row:    # handle empty lines errors
             sys.stderr.write('STDERR: empty row, script will continue running.\n')
             continue
-
         else:
-            row[0] = date_change(row[0])    # call 'date_change' function, modify 0 column
-            row[1] = get_country_code(row[1])   # call 'get_country_code' function, modify 1st column
-            row[2] = impress_check(row[2])      # call 'impress_check' function, modity 2nd column
-            row[3] = calculate_clicks(row[2],row[3])     # call 'calculate_clicks' function, modify 3rd column
-            input_Rows.append(row)      # append modified row into list
-
+            row[0] = date_change(row[0])                # call 'date_change' function, modify 0 column
+            row[1] = get_country_code(row[1])           # call 'get_country_code' function, modify 1st column
+            row[2] = impress_check(row[2])              # call 'impress_check' function, modity 2nd column
+            row[3] = calculate_clicks(row[2],row[3])    # call 'calculate_clicks' function, modify 3rd column
+            input_Rows.append(row)                      # append modified row into list
 except csv.Error as error:
     sys.stderr.write('STDERR: File {}, line {}: {}. This script supports only UTF-8 encoding.\n'
                      'Output file can\'t be created.\n'.format(input_path, input_Reader.line_num, error))
     sys.exit()
 
-
-input_CSV.close()   # close input file
-
-
+input_CSV.close()           # close input file
 print('Reading and modification of .csv file is complete.')
-
 
 '''Sort lexicographically rows in input_Rows list and
 write output.csv file using csv writer from Python standard library,
 use lineterminator to create Unix line endings.'''
 
-
-input_Rows.sort()   # sort rows lexicographically
-
-
-# create 'output' dir in CWD and create file path
-
-output_DIR = 'output'
+input_Rows.sort()           # sort rows lexicographically
+output_DIR = 'output'       # create 'output' dir in CWD and create file path
 os.makedirs(output_DIR, exist_ok=True)
 output_path = os.path.join((output_DIR), 'output.csv')
-
-
 print('Create {} file.\n'.format(output_path))
 
-
 output_CSV = open(output_path, 'w', newline='', encoding='utf-8')    # create output file in utf-8 encoding
-output_Writer = csv.writer(output_CSV, lineterminator='\n')     # make unix line endings
+output_Writer = csv.writer(output_CSV, lineterminator='\n')          # make unix line endings
 
-
-# iterate trough input_Rows modified list
-for row in input_Rows:
-
+for row in input_Rows:      # iterate trough input_Rows modified list
     output_Writer.writerow(row)
 
-output_CSV.close()  # close output file
-
-
-# End credits
-print(' Writing output file is complete '.center(100, '*'))
+output_CSV.close()          # close output file
+print(' Writing output file is complete '.center(100, '*'))          # End credits
 print(' Thank you for using my script! '.center(100, '*'))
 print(' Krzysztof Brymer '.center(100, '*'))
--- functions.py
+++ functions.py
@@ -4,101 +4,72 @@ import pycountry, sys
 
 
 def date_change(date):
-
     '''This function creates a new date in expected output format
     and detects errors in given "date" parameter.
 
     Example: MM/DD/YYYY to YYYY-MM-DD'''
-
-    # detect empty 'date' parameter
-    if not date:
+    if not date:            # detect empty 'date' parameter
         sys.stderr.write('STDERR: Date not found. Script will continue.\n')
 
         return 'XXXX-XX-XX'
-
-    # detect date format
-    elif len(date) != 10:
+    elif len(date) != 10:   # detect date format
         sys.stderr.write('STDERR: Date format invalid. Script will continue.\n')
         return 'XXXX-XX-XX'
-
-    # change date format
-    else:
+    else:                   # change date format
         new_date = date.split('/')
         new_date.insert(0, new_date.pop(-1))    # change position of the year to 0 position in list
-        new_date = '-'.join(new_date)   # create YYYY-MM-DD format
-
+        new_date = '-'.join(new_date)           # create YYYY-MM-DD format
         return new_date
 
 
 def get_country_code(name):
-
     '''This function searches for aplha_3 country code (DEU, POL, GIN)
     by using name of subdivision as a parameter, and detects errors.
 
     Example: Berlin to DEU'''
-
-    # detect empty 'name' parameter.
-    if not name:
+    if not name:            # detect empty 'name' parameter.
         sys.stderr.write('STDERR: Error: subdivision name field is empty.\n')
         return 'XXX'
-
-    # search for country code
-    else:
+    else:                   # search for country code
         for subdiv in list(pycountry.subdivisions):     # iterate trough subdivision list
             if name in subdiv.name:
-                code = subdiv.country_code      # code is also alpha_2 code of a country
+                code = subdiv.country_code              # code is also alpha_2 code of a country
                 country = pycountry.countries.get(alpha_2 = code)
                 return country.alpha_3
 
-        return 'XXX'    # if function didn't find nothing, return 'XXX'
+        return 'XXX'        # if function didn't find nothing, return 'XXX'
 
 
 def impress_check(impress):
-
     '''This function checks impressions column format
     for errors and invalid formats.
 
     Example: Unknown to 0'''
-
-    # detect empty impress field
-    if not impress:
+    if not impress:                 # detect empty impress field
         sys.stderr.write('STDERR: Error: impressions field is empty.\n')
-
         return '0'
-
-    # detect invalid parameter type
-    elif type(impress) == 'str':
+    elif type(impress) == 'str':    # detect invalid parameter type
         sys.stderr.write('STDERR: Error: impressions field format invalid.\n')
-
         return '0'
-
     else:
         return impress
 
 
 def calculate_clicks(impress, CTR):
-
     '''This function calculate number of clicks from CTR (Click To
      impression Rate) and number of impressions. Also detects errors.
 
      Example: 916, 0.67% to 6'''
-
-    # detect empty 'CTR' parameter.
-    if not impress or not CTR:
+    if not impress or not CTR:      # detect empty 'CTR' parameter.
         sys.stderr.write('STDERR: Error: CTR field is empty. Can\'t calculate clicks.\n')
 
         return '0'
-
-    # calculate clicks
-    else:
+    else:                           # calculate clicks
         try:
             a = float(CTR[:-1])
             b = float(impress)
             clicks = b * (a/100)
-
             return round(clicks)
-
         except ValueError as error:
             sys.stderr.write('STDERR: Error calculating clicks, {}\n'.format(error))
-
             return '0'
